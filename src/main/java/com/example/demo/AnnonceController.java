b45cc541b4675091369afe357069db26651e6a30
larakii1 <abdelghafourelkanbi@gmail.com>
2019-10-30 12:09:30 +0100

    creationd de annonceController , annonce service et configuration d'application properties , creation base de données annonce psql

---
+++ src/main/java/com/example/demo/AnnonceController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class AnnonceController {
+@Autowired
+AnnonceService annonceservice;
+
+}
